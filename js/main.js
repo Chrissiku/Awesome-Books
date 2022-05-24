08a5547b7e8df1b2ea26478c8d730fce42621ec2
chrissiku <chrissiku5@gmail.com>
2022-05-24 16:43:55 +0200

    Fix linters errors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Declaration of my items in he DOM
-const bookTitle = document.querySelector("#book-title");
-const bookAuthor = document.querySelector("#book-author");
-const addBookBtn = document.querySelector("#add");
-const bookList = document.querySelector("#bookList");
+const bookTitle = document.querySelector('#book-title');
+const bookAuthor = document.querySelector('#book-author');
+const addBookBtn = document.querySelector('#add');
+const bookList = document.querySelector('#bookList');
 
 // Initialise the collection of books
-let collectBooks = JSON.parse(localStorage.getItem("books")) || [];
+let collectBooks = JSON.parse(localStorage.getItem('books')) || [];
 
 // Function to add Book to the Collection of books, here callectBooks
 function addBook() {
@@ -21,31 +21,31 @@ function remove(element) {
   const bookToDelete = collectBooks[bookId - 1];
   const freshCollection = collectBooks.filter((book) => book !== bookToDelete);
   collectBooks = freshCollection;
-  localStorage.setItem("books", JSON.stringify(freshCollection));
+  localStorage.setItem('books', JSON.stringify(freshCollection));
   element.target.parentElement.remove();
 }
 
 // Function to display all books
 function displayBooks() {
-  bookList.innerHTML = "";
+  bookList.innerHTML = '';
   collectBooks.forEach((element, index) => {
     // Define all neccessary items
-    const parentContainer = document.createElement("div");
-    parentContainer.classList.add("book-card");
-    const titleContainer = document.createElement("span");
-    const authorContainer = document.createElement("span");
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("btn");
-    const bookInfos = document.createElement("p");
-    bookInfos.classList.add("book-infos");
-    removeButton.innerText = "Remove";
+    const parentContainer = document.createElement('div');
+    parentContainer.classList.add('book-card');
+    const titleContainer = document.createElement('span');
+    const authorContainer = document.createElement('span');
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('btn');
+    const bookInfos = document.createElement('p');
+    bookInfos.classList.add('book-infos');
+    removeButton.innerText = 'Remove';
     // Remove book button on Click
-    removeButton.addEventListener("click", (e) => {
+    removeButton.addEventListener('click', (e) => {
       remove(e);
     });
 
-    removeButton.setAttribute("id", index + 1);
-    titleContainer.innerText = "'' " + element.title + " '' by ";
+    removeButton.setAttribute('id', index + 1);
+    titleContainer.innerText = `'' ${element.title} '' by `;
     authorContainer.innerText = element.author;
     bookInfos.appendChild(titleContainer);
     bookInfos.appendChild(authorContainer);
@@ -56,14 +56,14 @@ function displayBooks() {
   });
 }
 // Button to add new book to the collection
-addBookBtn.addEventListener("click", () => {
+addBookBtn.addEventListener('click', () => {
   addBook();
-  bookTitle.value = "";
-  bookAuthor.value = "";
-  localStorage.setItem("books", JSON.stringify(collectBooks));
+  bookTitle.value = '';
+  bookAuthor.value = '';
+  localStorage.setItem('books', JSON.stringify(collectBooks));
   displayBooks();
 });
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   displayBooks();
 });
